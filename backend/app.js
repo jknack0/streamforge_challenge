306eb521ce93d2906b49c11caf748c55750f48a4
jknack0 <jknack@mail.sfsu.edu>
2020-12-02 15:53:06 -0800

    removed a log

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,13 @@ const middleware = require('./utils/middleware')
 const channelRouter = require('./controllers/channel')
 const apicache = require('apicache')
 const cache = apicache.middleware
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(cache('5 minutes'))
 
-app.use('/api/streamer', channelRouter)
+app.use('/api/channel', channelRouter)
 
 app.use(middleware.unknownEndpoint)
 
