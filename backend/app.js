44a8cd85361ae4c2e404f976c7a95c517e1837e3
jknack0 <jknack@mail.sfsu.edu>
2020-12-02 12:18:22 -0800

    added cache

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const middleware = require('./utils/middleware')
 const channelRouter = require('./controllers/channel')
+const apicache = require('apicache')
+const cache = apicache.middleware
 
 app.use(express.json())
+app.use(cache('5 minutes'))
 
 app.use('/api/streamer', channelRouter)
 
